10c93c060b3fcbf146454690bcce2cdf859937a9
asangalyevas69 <sabinaasangalyeva34@gmail.com>
2025-04-23 14:56:08 +0600

    models

--- apps/students/models.py
+++ apps/students/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='students/')
+
+class Attendance(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    date = models.DateField()
+    status = models.CharField(max_length=10, choices=[('Present', 'Present'), ('Absent', 'Absent')])
+
+class Parent(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+
+class Lesson(models.Model):
+    subject = models.CharField(max_length=100)
+    date = models.DateField()
+
--- apps/students/views.py
+++ apps/students/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
-# Create your views here.
+# from rest_framework import viewsets
+# from .models import Student, Attendance, Parent
+# # from .serializers import StudentSerializer, AttendanceSerializer, ParentSerializer
+#
+# class StudentViewSet(viewsets.ModelViewSet):
+#     queryset = Student.objects.all()
+#     serializer_class = StudentSerializer
+#
+# class AttendanceViewSet(viewsets.ModelViewSet):
+#     queryset = Attendance.objects.all()
+#     serializer_class = AttendanceSerializer
+#
+# class ParentViewSet(viewsets.ModelViewSet):
+#     queryset = Parent.objects.all()
+#     serializer_class = ParentSerializer
